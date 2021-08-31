838f0e4d04907d8d70a1d5ec7b7b1ab3fc1e5540
Tambi <tambi.asawo@yahoo.com>
2021-08-31 07:12:29 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
-
+import React from "react";
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<React.StrictMode> <App /> </React.StrictMode>, document.getElementById('root'));
